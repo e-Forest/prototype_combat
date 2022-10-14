0383874873cdc3feb9fc1da6ede4ae24507cd6e2
e-Forest <ewalddojan@gmail.com>
2022-10-14 17:00:16 +0200

    fix npc nav_agent

--- src/npc.gd
+++ src/npc.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
-
-const SPEED = 50.0
+#const SPEED = 50.0
 @onready var nav_agent = $NavAgent as NavigationAgent2D
 
 var player:Player
@@ -11,12 +10,17 @@ func _ready():
 	nav_agent.velocity_computed.connect(_on_velocity_computed)
 
 func _physics_process(delta):
-	nav_agent.set_target_location(player.global_position)
-	if nav_agent.is_target_reachable():
-		var next_location = nav_agent.get_next_location()
-		nav_agent.set_velocity(global_position.direction_to(next_location) * SPEED)
+#	if nav_agent.is_target_reachable():
+	var next_location = nav_agent.get_next_location()
+	nav_agent.set_velocity(global_position.direction_to(next_location) * nav_agent.max_speed)
+#	else:
+#		nav_agent.set_velocity(Vector2.ZERO)
 
 
 func _on_velocity_computed(safe_velocity):
 	velocity = safe_velocity
 	move_and_slide()
+
+
+func _on_timer_timeout():
+	nav_agent.set_target_location(player.global_position)
